80f3b38e86bf8d1fc4854c4caaa47b10f2528f43
Mokammel Tanvir <mokammeltanvir01@gmail.com>
2023-06-17 01:50:01 +0600

    Breeze auth setup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +19,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::factory(1)->create();
+            
     }
 }
