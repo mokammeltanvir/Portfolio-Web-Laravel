977b79d8f6bc6b37bf7d227739244491a2adc2d8
Mokammel Hossain Tanvir <mokammeltanvir01@gmail.com>
2024-03-03 21:39:34 +0600

    bug fix

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -3,6 +3,7 @@
 
 <head>
   <meta charset="UTF-8">
+  <meta name="csrf-token" content="{{ csrf_token() }}" />
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
   <title>Admin | @yield('title')</title>
 
@@ -21,7 +22,7 @@
       <div class="main-content">
         @yield('content')
       </div>
-      
+
       <!-- include footer -->
       @include('admin.layouts.inc.footer')
     </div>
